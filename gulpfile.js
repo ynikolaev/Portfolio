452cdea08e2bc1be8161ecda952819432657233e
Yan Nikolaev <yannikolaev@gmail.com>
2018-08-07 07:53:43 +0100

    Gulpfile and its dependencies

---
+++ gulpfile.js
@@ -0,0 +1,6 @@
+const gulp = require('gulp'),
+gutil = require('gulp-util');
+//now TASKS can be assigned using Gulp library
+gulp.task('log', function () {
+    gutil.log("Portfolio in progress");
+});
